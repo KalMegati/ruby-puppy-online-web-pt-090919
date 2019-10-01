a27c306f84645c009f5a2c006f363a9c98ecb6ac
Srikalyan Megati <kalmegati@gmail.com>
2019-10-01 18:25:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
+require 'pry'
+
 # Add your code here
+
+class Dog
+  
+  @@all = []
+  
+  def save
+    @@all << self
+  end
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    puts @@all.collect { |instance| instance.name }
+  end
+  
+  
+  
+end
